c669e2851e22d8d888f9c3870144666fcb6d5c7c
Amy_Li <38838834+luckmimi@users.noreply.github.com>
2021-12-29 07:28:59 -0800

    Create LC215.py

---
+++ LC215.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = nums[:k]
+        heapify(heap)
+        for n in nums[k:]:
+            heappushpop(heap,n)
+        return heap[0]
