2f6c8f4c7fd30eb9200207ceae70736951cdc48d
Amy_Li <38838834+luckmimi@users.noreply.github.com>
2022-02-28 12:00:49 -0800

    Create LC163.py

---
+++ LC163.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
+        def format(lower,upper):
+            if lower == upper:
+                return str(lower)
+            return f'{lower}->{upper}'
+        
+        result = []
+        prev = lower - 1
+        for i in range(len(nums)+1):
+            curr = nums[i] if i <len(nums) else upper + 1
+            if prev + 1 <= curr - 1:
+                result.append(format(prev+1,curr - 1))
+            prev = curr
+        return result
+                
