6f4bfda832f69e1737fdda2a05c6968e8934af61
Amy_Li <38838834+luckmimi@users.noreply.github.com>
2022-03-09 16:06:55 -0800

    Create LC116.py

---
+++ LC116.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root: return root
+        cur = root
+        while cur:
+            head = cur
+            while cur:
+                if cur.left:
+                    cur.left.next = cur.right
+                if cur.right and cur.next:
+                    cur.right.next = cur.next.left
+                cur = cur.next
+            cur = head.left
+        return root
+        
