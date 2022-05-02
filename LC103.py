d5677bcd73c9433e3cc1200f4071c3a124430b98
Amy_Li <38838834+luckmimi@users.noreply.github.com>
2022-05-02 13:28:15 -0700

    Create LC103.py

---
+++ LC103.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q = collections.deque([root])
+        res = []
+        flag = 1
+        while q:
+            n = len(q)
+            level = []
+            for i in range(n):
+                cur = q.popleft()
+                level.append(cur.val)
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+            res.append(level[::flag])
+            flag *= -1
+        return res
+                    
+                    
+            
+            
+        
