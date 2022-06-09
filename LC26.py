08810c6324c53607ff4a8a6add2bb269d00af003
Amy_Li <38838834+luckmimi@users.noreply.github.com>
2022-06-09 14:56:21 -0700

    Create LC26.py

---
+++ LC26.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 0: return 0 
+        slow, fast = 0, 0
+        while fast < len(nums):
+            if nums[slow] != nums[fast]:
+                slow += 1
+                nums[slow] = nums[fast]
+                
+            fast += 1
+        return slow + 1
+                
